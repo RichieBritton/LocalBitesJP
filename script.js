b674e4cc1d80fff0625e0865b4478a079a4d17f5
Richie Britton <96446339+RichieBritton@users.noreply.github.com>
2024-01-11 00:38:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// app.js
+<a class="navbar-brand" href="#" onclick="location.reload(); return false;">
+  <div class="logo-grid">
+    <img src="LocalBites.png" alt="Logo" id="logo">
+  </div>
+</a>
